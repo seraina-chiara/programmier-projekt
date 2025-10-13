cac1863f753ebfd2856e25f76249d0e2e0d2a05f
seraina-chiara <seraina.meister@students.fhnw.ch>
2025-10-13 11:31:17 +0200

    done delete set and card

--- main.py
+++ main.py
@@ -1,112 +1,36 @@
+import manageFiles
 import os
 
-# Mateo
-def create_set():
-    print(f"{'-'*40}")
-    print(f"{'Set ERSTELLEN':^40}")
-    print(f"{'-'*40}")
 
-# Gui
-def edit_cards():
-    print(f"{'-'*40}")
-    print(f"{'Set oder Karte BEARBEITEN':^40}")
-    print(f"{'-'*40}")
+# -----------------------------
+# Hauptmenü anzeigen
+# -----------------------------
+def show_menu():
+    print("\nWähle aus zwischen diesen Optionen:")
+    print("\t1. Neues Karteikartenset anlegen")
+    print("\t2. Bestehende Karten aus einem Set bearbeiten")
+    print("\t3. Bestehende Karten aus einem Set löschen")
+    print("\t4. Bestehendes Set üben")
+    print("Geben Sie ein -1 ein um das Programm zu beenden")
 
-# Dimitrjie
-def learn_set():
-    print(f"{'-'*40}")
-    print(f"{'Set LERNEN':^40}")
-    print(f"{'-'*40}")
-    
-# Seraina
-def delete_cards():
-    print(f"{'-'*40}")
-    print(f"{'Set oder Karte LÖSCHEN':^40}")
-    print(f"{'-'*40}")
 
+# -----------------------------
+# Auswahl im Hauptmenü
+# -----------------------------
+def choose_option():
     while True:
+        show_menu()
         try:
-            option = int(input("Wollen Sie ein Set löschen (1)\noder eine Karte aus einem Set löschen (2)\nUm das Programm abbzubrechen gebe -1 ein: "))
-            if  option == 1:
-                print("Welches Set soll gelöscht werden?")
-                selected_file = read_all_sets()
-                if os.path.exists(selected_file):
-                    os.remove(selected_file)
-                    print(f"Set '{selected_file}' wurde gelöscht.")
-                else:
-                    print(f"Set '{selected_file}' existiert nicht.")
-            elif(option == 2):
-                print("Aus welchem Set soll eine Karte gelöscht werden?")
-                selected_file = read_all_sets()
-                # TODO: delete one card out of set
-            elif(option == -1):
-                break
+            option = int(input("Welche dieser Optionen möchten sie machen? "))
         except ValueError:
-            print("Das war keine gültige Auswahl")
-    
-    
-    print("delete set")
-    
-def read_all_sets():
-    # Ordnerpfad
-    folder = "sets"
-    # Alle Dateien im Ordner abrufen
-    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
-    if not files:
-        print("Im Ordner 'sets' wurden keine Dateien gefunden.")
-    else:
-        # alle Optionen der Dateien ausgeben
-        print("Verfügbare Dateien:")
-        for i, file in enumerate(files, start=1):
-            print(f"{i}. {file}")
+            print("Diese Option ist nicht valid")
+            continue
 
-        while True:
-            try:
-                # Auswahl eines bestimmten Sets
-                choice = int(input(f"Geben Sie das gewünschte Set an (Nummer zwischen 1 und {len(files)}): "))
-                if 1 <= choice <= len(files):
-                    selected_file = files[choice - 1]
-                    print(f"\nDu hast '{selected_file}' ausgewählt.")
-                    return folder + '/' + selected_file
-                else:
-                    print("Ungültige Nummer. Bitte erneut versuchen.")
-            except ValueError:
-                print("Bitte eine Zahl eingeben")
+        # close application if user gives -1
+        if option == -1:
+            print("Programm wird beendet.")
+            break
 
-
-
- # Introduce User   
-print(f"{'-'*40}")
-print(f"{'MemoCards':^40}")
-print(f"{'-'*40}")
-
-# Give the user its options
-print("\nWähle aus zwischen diesen Optionen:")
-print("\t1. Neues Karteikartenset anlegen")
-print("\t2. Bestehende Karten aus einem Set bearbeiten")
-print("\t3. Bestehende Karten aus einem Set löschen")
-print("\t4. Bestehendes Set üben")
-print("Geben Sie ein -1 ein um das Programm zu beenden")
-
-while True:
-    try:
-        option = int(input("Welche dieser Optionen möchten sie machen? "))
-    except ValueError:
-        print("Diese Option ist nicht valid")
-        continue
-
-    # close application if user gives -1
-    if option == -1:
-        print("Programm wird beendet.")
-        break
-
-    # Validate Option
-    # option has to be a digit and between 1 and 4
-    if 1 <= int(option) <= 4:
-
-        print(f"Du hast die Option {option} gewählt!")
-        option = int(option)
-        
         if(option == 1):
             create_set()
         elif(option == 2):
@@ -115,5 +39,110 @@ while True:
             delete_cards()
         elif(option == 4):
             learn_set()
-    else: 
-        print("Das ist keine gültige Auswahl")
+        else: 
+            print("Das ist keine gültige Auswahl")
+
+# -----------------------------
+# Option 1 – Neues Set erstellen
+# -----------------------------
+# Mateo
+def create_set():
+    print_title("Neues Set erstellen")
+
+# -----------------------------
+# Option 2 – Karten bearbeiten
+# ----------------------------
+# Gui
+def edit_cards():
+    print_title("Set oder Karte bearbeiten")
+
+# -----------------------------
+# Option 3 – Set oder Karte löschen
+# -----------------------------
+# Seraina
+def delete_cards():
+    print_title("Set oder Karte löschen")
+
+    while True:
+        try:
+            print("\n1. Ganzes Set löschen")
+            print("2. Einzelne Karte löschen")
+            print("-1. Zurück zum Hauptmenü")
+
+            choice = int(input("Deine Auswahl: "))
+
+            if choice == -1:
+                break
+
+            elif choice == 1:
+                print("Welches Set soll gelöscht werden?")
+                selected_file = manageFiles.read_all_sets()
+
+                if selected_file and os.path.exists(selected_file):
+                    if get_yes_or_no("Sind Sie sicher?"):
+                        os.remove(selected_file)
+                        print(f"Set '{selected_file}' wurde gelöscht.")
+                else:
+                    print(f"Set '{selected_file}' existiert nicht.")
+
+                
+            elif choice == 2:
+                print("Aus welchem Set soll eine Karte gelöscht werden?")
+                selected_file = manageFiles.read_all_sets()
+
+                if selected_file is None:
+                    print("Es konnte kein Set ausgewählt werden")
+                    break
+
+                selected_card = manageFiles.select_card_from_set(selected_file)
+
+                if selected_card is None:
+                    print("Es konnte keine Karte ausgewählt werden")
+                    break
+
+                if get_yes_or_no("Sind Sie sicher?"):
+                    with open(str(selected_file), 'r', encoding="utf-8") as fs:
+                        lines = fs.readlines()
+                    del lines[selected_card]
+                    with open(str(selected_file), 'w', encoding="utf-8") as fs:
+                        fs.writelines(lines)
+                
+            else:
+                print("Das war keine gültige Auswahl")
+        except ValueError:
+            print("Das war keine gültige Auswahl")
+
+  
+# -----------------------------
+# Option 4 – Lernen
+# -----------------------------
+# Dimitrjie
+def learn_set():
+    print_title("Set lernen")
+    
+ 
+# -----------------------------
+# Hilfsfunktion: Titel hübsch drucken
+# -----------------------------
+def print_title(title):
+    print("\n" + "-" * 40)
+    print(title.center(40))
+    print("-" * 40)
+
+def get_yes_or_no(question):
+    while True:
+        answer = input(f"{question} (j/n): ")
+        if answer == 'j':
+            return True
+        elif answer == 'n':
+            return False
+        else:
+            print("Bitte geben sie j oder n ein")
+
+def main():
+    print_title("MemoCards")
+    choose_option()
+
+
+if __name__ == '__main__':
+    main()
---
+++ manageFiles.py
@@ -0,0 +1,46 @@
+import os
+
+def read_all_sets():
+    # Ordnerpfad
+    folder = "sets"
+    # Alle Dateien im Ordner abrufen
+    files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+    if not files:
+        print(f"Im Ordner '{folder}' wurden keine Dateien gefunden.")
+    else:
+        # alle Optionen der Dateien ausgeben
+        print("Verfügbare Dateien:")
+        for i, file in enumerate(files, start=1):
+            print(f"\t{i}. {file}")
+
+        while True:
+            try:
+                # Auswahl eines bestimmten Sets
+                choice = int(input(f"Geben Sie das gewünschte Set an (Nummer zwischen 1 und {len(files)}): "))
+                if 1 <= choice <= len(files):
+                    selected_file = files[choice - 1]
+                    print(f"\nDu hast '{choice - 1}. {selected_file}' ausgewählt.")
+                    return folder + '/' + selected_file
+                else:
+                    print("Ungültige Nummer. Bitte erneut versuchen.")
+            except ValueError:
+                print("Bitte eine Zahl eingeben")
+
+def select_card_from_set(selected_file):
+    with open(str(selected_file), 'r', encoding="utf-8") as fs:
+        lines = fs.readlines()
+
+    for i, line in enumerate(lines, start=1):
+        print(f"\t{i}. {line.strip()}")
+
+    while True:
+        try:
+            choice = int(input(f"\nGeben Sie die gewünschte Karte an (Nummer zwischen 1 und {len(lines)}): "))
+            if 1 <= choice <= len(lines):
+                selected_card = lines[choice - 1].strip()
+                print(f"\nDu hast '{choice}. {selected_card}' ausgewählt.")
+                return choice - 1
+            else:
+                print("Ungültige Nummer. Bitte erneut versuchen.")
+        except ValueError:
+                print("Bitte eine Zahl eingeben")
